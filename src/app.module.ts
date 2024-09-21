a93a2f43ff68c21e990ee03dd5c70bcb4d014a03
Usman Tayyab <usman.bestdev@gmail.com>
2024-09-21 19:50:21 +0500

    get all the database connection values from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,33 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from './company/company.module';
 import { ServiceFeeModule } from './service-fee/service-fee.module';
+import { ConfigModule,ConfigService } from '@nestjs/config';
+import { Seeder } from 'typeorm-extension'; // Add this import
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',  // the new user you created
-      password: 'mysecretpassword',
-      database: 'company-management-db',
-      autoLoadEntities: true,
-      synchronize: true,
+     // Load environment variables from .env file
+     ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     CompanyModule,
-    ServiceFeeModule
+    ServiceFeeModule,
+
   ],
 })
 export class AppModule {}

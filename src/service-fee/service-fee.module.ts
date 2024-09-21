18843b70407e276904ae5a12cf63f21b232c213c
Usman Tayyab <usman.bestdev@gmail.com>
2024-09-21 18:02:45 +0500

    added create fee endpoint

--- src/service-fee/service-fee.module.ts
+++ src/service-fee/service-fee.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceFee } from 'src/entities/service-fee/service-fee.entity';
+import { Company } from 'src/entities/company/company.entity'; 
 import { ServiceFeeController } from './service-fee.controller';
 import { ServiceFeeService } from './service-fee.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ServiceFee])],
+  imports: [TypeOrmModule.forFeature([ServiceFee, Company])], 
   controllers: [ServiceFeeController],
   providers: [ServiceFeeService],
 })
--- src/service-fee/service-fee.service.ts
+++ src/service-fee/service-fee.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from 'src/entities/company/company.entity';
 import { ServiceFee } from 'src/entities/service-fee/service-fee.entity';
 import { Repository } from 'typeorm';
 
@@ -8,12 +9,16 @@ export class ServiceFeeService {
   constructor(
     @InjectRepository(ServiceFee)
     private serviceFeeRepository: Repository<ServiceFee>,
+
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
   ) {}
 
-  findAll(): Promise<ServiceFee[]> {
+  async findAll(): Promise<ServiceFee[]> {
     return this.serviceFeeRepository.find({ relations: ['company'] });
   }
-  findAllByCompany(companyId: string): Promise<ServiceFee[]> {
+
+  async findAllByCompany(companyId: string): Promise<ServiceFee[]> {
     const companyIdAsNumber = Number(companyId);
     return this.serviceFeeRepository.find({
       where: { company: { id: companyIdAsNumber } },
@@ -21,7 +26,20 @@ export class ServiceFeeService {
     });
   }
 
-  create(serviceFee: ServiceFee): Promise<ServiceFee> {
+  async create(serviceFee: ServiceFee): Promise<ServiceFee> {
+    // Find the company by ID before saving the service fee
+    const company = await this.companyRepository.findOne({
+      where: { id: serviceFee.company.id },
+    });
+
+    if (!company) {
+      throw new Error('Company not found');
+    }
+
+    // Assign the found company to the service fee
+    serviceFee.company = company;
+
+    // Save the service fee
     return this.serviceFeeRepository.save(serviceFee);
   }
 }
